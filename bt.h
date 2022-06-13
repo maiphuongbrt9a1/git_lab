1e08b938989738280da9ab5cc4801d2a1ab3ae0c
kenB <maiphuongbrt9a1@gmail.com>
2022-06-13 15:51:40 +0700

    lan 3

--- bt.h
+++ bt.h
@@ -8,6 +8,7 @@ using namespace std;
 
 long long int sum_n_number(long long int n)
 {
+    // tong cac so tu 1 den n
     long long int result = 0;
     for (long long int i = 1; i <= n; i++)
     {
@@ -133,4 +134,100 @@ float tong_can_long_nhau_35(int n)
         }
 }
 
-// phuong dep traj
+float tong_can_long_nhau_40(int x, int n)
+{
+    static int i = 1;
+    static int j = n;
+    if (i == j) return sqrt(x);
+    else
+    {
+        i++;
+        return sqrt(power_x_n(x, n) + tong_can_long_nhau_40(x, n - 1));
+    }
+}
+
+
+double tong_cau_41(int n)
+{
+    // tinh tong cua 1/
+    /*                1 + 1/
+                           1 + 1/.....
+    co n dau phan so */
+
+
+    static int start = 1;
+    static int end = n;
+
+    if (start == end) return 1.0 / 2;
+    else 
+    {
+        start ++;
+        return 1.0 / (1 + tong_cau_41(n));
+    }
+}
+
+void find_k_max(int n)
+{
+    int result = 0;
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        sum += i;
+        if (sum < n)
+        {
+            result++;
+        }
+        else
+        {
+            break;
+        }
+        
+    }   
+    cout << "cau 42 k max la: " << result << endl;
+}
+
+
+void number_of_digits(long long int n)
+{
+    string result = "";
+    result = std::to_string(n);
+    cout << "do dai cua so n la: " << result.length() << endl;
+}
+
+int sum_of_digits(int n)
+{
+    // tong cac chu so cua so n
+    int result = 0;
+    int length_number_str = 0;
+    string number_string = "";
+    number_string = std::to_string(n);
+    length_number_str = number_string.length();
+
+    for (int i = 0; i < length_number_str; i++)
+    {
+        result += (int(number_string[i]) - 48);
+    }
+     
+    return result;
+}
+
+
+long long int reverse(long long int n)
+{
+    // dao nguoc so n (reverse of the n) 
+    long long int result = 0;
+    int remainder = 0;
+    long long int x = n;
+
+    while (x != 0)
+    {
+        remainder = x % 10;
+        result = result * 10 + remainder;
+        x /= 10;
+    }
+    
+
+    return result;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -9,22 +9,33 @@ int main()
     cin >> n;
     cout << "nhap x:\n";
     cin >> x;
-    cout << sum_n_number(n) << endl;
-    cout << sum_square_n_number(n) << endl;
-    cout << tong_phan_so_1_chia_n(n) << endl;
-    cout << tong_phan_so_1_chia_2n(n) << endl;
-    cout << power_x_n(x, n) << endl;
+    // cout << sum_n_number(n) << endl;
+    // cout << sum_square_n_number(n) << endl;
+    // cout << tong_phan_so_1_chia_n(n) << endl;
+    // cout << tong_phan_so_1_chia_2n(n) << endl;
+    // cout << power_x_n(x, n) << endl;
 
 
-    // cout << "factorial of n:" << endl;
-    // cout << factorial_n(n) << endl;
+    // // cout << "factorial of n:" << endl;
+    // // cout << factorial_n(n) << endl;
 
-    cout << "tong s_n: \n";
-    cout << sum_sn(x,n) << endl;
+    // cout << "tong s_n: \n";
+    // cout << sum_sn(x,n) << endl;
 
-    uoc_so_cua_n(n);
-    cout << "tong cua s_n_33: " << tong_can_2_long_nhau(n) << endl;
-    cout << "tong cua s_n_34: " << tong_can_long_nhau_34(n) << endl;
-    cout << "tong cua s_n_35: " << tong_can_long_nhau_35(n) << endl;
+    // uoc_so_cua_n(n);
+    // cout << "tong cua s_n_33: " << tong_can_2_long_nhau(n) << endl;
+    // cout << "tong cua s_n_34: " << tong_can_long_nhau_34(n) << endl;
+    // cout << "tong cua s_n_35: " << tong_can_long_nhau_35(n) << endl;
+
+    // cout << "tong cua s_n_40: " << tong_can_long_nhau_40(x, n) << endl;
+
+
+    // cout << "tong cau 41: " << tong_cau_41(n) << endl;
+    // find_k_max(n);
+
+    // number_of_digits(n);
+    // cout << "tong cac chu so cua so n: " << sum_of_digits(n) << endl;
+    
+    cout << "reverse of n: " << reverse(n) << endl;
     return 0;
 }
